159646257bf3f2005872eb359ddafa7eb23595ae
ksurbhi <94086790+ksurbhi@users.noreply.github.com>
2025-02-26 12:25:32 -0800

    167. Two Sum II - Input Array Is Sorted

---
+++ twoSum.py
@@ -0,0 +1,27 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # using 2 Pointers
+        start = 0
+        end = len(numbers)-1
+        while start <= end:
+            sum = numbers[start] + numbers[end]
+            if sum == target:
+                return [start+1, end+1]
+            elif sum < target:
+                start += 1
+            else:
+                end -= 1
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # Using Hashmap
+        # my_dict = {}
+        # for i  in range(len(numbers)):
+        #     diff = target - numbers[i] 
+        #     if diff in my_dict:
+        #         return[my_dict[diff],i+1]
+        #     my_dict[numbers[i]] = i + 1 
+        
+            
+        
